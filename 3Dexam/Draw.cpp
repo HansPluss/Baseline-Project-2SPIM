e9d3a956bcb934bc0fc5168debfb5b5efcefa76b
Hans <111863567+HansPluss@users.noreply.github.com>
2024-09-01 14:29:37 +0200

    Fixed Indices rendering wrong

--- 3Dexam/Draw.cpp
+++ 3Dexam/Draw.cpp
@@ -17,16 +17,15 @@ void Draw::DrawCube(glm::vec3 Color, glm::vec3 pos, glm::vec3 size)
     glm::vec3 sizeXYZ = glm::vec3(1.f, 1.f, 1.f);
 
     // Front face vertices
-    Vertex v0{ -sizeXYZ.x, -sizeXYZ.y, sizeXYZ.z , Color.x, Color.y, Color.z, 0.0f, 1.0f };
-    Vertex v1{ sizeXYZ.x, -sizeXYZ.y, sizeXYZ.z, Color.x, Color.y, Color.z, 1.0f, 1.0f };
-    Vertex v2{ sizeXYZ.x, sizeXYZ.y, sizeXYZ.z , Color.x, Color.y, Color.z, 1.0f, 0.0f };
-    Vertex v3{ -sizeXYZ.x, sizeXYZ.y, sizeXYZ.z ,  Color.x, Color.y, Color.z, 0.0f, 0.0f };
+    Vertex v0{ -sizeXYZ.x, -sizeXYZ.y,  sizeXYZ.z , Color.x, Color.y, Color.z, 0.0f, 1.0f };
+    Vertex v1{ sizeXYZ.x, -sizeXYZ.y,  sizeXYZ.z , Color.x, Color.y, Color.z, 1.0f, 1.0f };
+    Vertex v2{ sizeXYZ.x,  sizeXYZ.y,  sizeXYZ.z , Color.x, Color.y, Color.z, 1.0f, 0.0f };
+    Vertex v3{ -sizeXYZ.x,  sizeXYZ.y,  sizeXYZ.z , Color.x, Color.y, Color.z, 0.0f, 0.0f };
 
-    // Right face vertices
-    Vertex v4{ -sizeXYZ.x, -sizeXYZ.y, sizeXYZ.z, Color.x, Color.y, Color.z, 0.0f, 1.0f };
-    Vertex v5{ sizeXYZ.x, -sizeXYZ.y, -sizeXYZ.z, Color.x, Color.y, Color.z, 1.0f, 1.0f };
-    Vertex v6{ sizeXYZ.x, sizeXYZ.y, sizeXYZ.z , Color.x, Color.y, Color.z, 1.0f, 0.0f };
-    Vertex v7{ -sizeXYZ.x, sizeXYZ.y, -sizeXYZ.z , Color.x, Color.y, Color.z, 0.0f, 0.0f };
+    Vertex v4{ -sizeXYZ.x, -sizeXYZ.y, -sizeXYZ.z , Color.x, Color.y, Color.z, 0.0f, 1.0f };
+    Vertex v5{ sizeXYZ.x, -sizeXYZ.y, -sizeXYZ.z , Color.x, Color.y, Color.z, 1.0f, 1.0f };
+    Vertex v6{ sizeXYZ.x,  sizeXYZ.y, -sizeXYZ.z , Color.x, Color.y, Color.z, 1.0f, 0.0f };
+    Vertex v7{ -sizeXYZ.x,  sizeXYZ.y, -sizeXYZ.z , Color.x, Color.y, Color.z, 0.0f, 0.0f };
 
     
          vertices = {
@@ -39,20 +38,24 @@ void Draw::DrawCube(glm::vec3 Color, glm::vec3 pos, glm::vec3 size)
         v6, // Back top right
         v7  // Back top left
     };
-    indices = {
-        // Front face
-        0, 1, 2, 2, 3, 0,
-        // Back face
-        4, 5, 6, 6, 7, 4,
-        // Left face
-        4, 0, 3, 3, 7, 4,
-        // Right face
-        1, 5, 6, 6, 2, 1,
-        // Top face
-        3, 2, 6, 6, 7, 3,
-        // Bottom face
-        4, 5, 1, 1, 0, 4
-    };
+
+    
+
+    // Corrected indices
+         indices = {
+             // Front face
+             0, 1, 2, 2, 3, 0,
+             // Back face
+             4, 5, 6, 6, 7, 4,
+             // Left face
+             4, 0, 3, 3, 7, 4,
+             // Right face
+             1, 5, 6, 6, 2, 1,
+             // Top face
+             3, 2, 6, 6, 7, 3,
+             // Bottom face
+             4, 5, 1, 1, 0, 4
+         };
 
     this->Initalize();
 }
@@ -69,23 +72,23 @@ std::vector<Vertex> Draw::DrawSphere(glm::vec3 Color, glm::vec3 pos, glm::vec3 s
 void Draw::Initalize()
 {
     //bind the VAO And VBO
-
-    EBO1.EBOSetUp(indices); 
-
+     // Bind the VAO
     VAO.Bind();
-    VBO.Bind();
-    EBO1.Bind();
 
+    // Bind the VBO and upload vertex data
+    VBO.Bind();
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
 
+    // Set vertex attributes pointers
+    VAO.LinkAttrib(VBO, 0, 3, GL_FLOAT, sizeof(Vertex), (void*)offsetof(Vertex, x)); // Position
+    VAO.LinkAttrib(VBO, 1, 3, GL_FLOAT, sizeof(Vertex), (void*)offsetof(Vertex, r)); // Color
+    VAO.LinkAttrib(VBO, 2, 2, GL_FLOAT, sizeof(Vertex), (void*)offsetof(Vertex, u)); // TexCoords
 
+    // Bind the EBO and upload index data
+    EBO1.Bind();
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
-    //link vertex position, color, textrue coordinates
-    VAO.LinkAttrib(VBO, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
-    VAO.LinkAttrib(VBO, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    VAO.LinkAttrib(VBO, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 
-    //unbind
+    // Unbind VAO, VBO, EBO
     VAO.Unbind();
     VBO.Unbind();
     EBO1.Unbind();
@@ -122,8 +125,8 @@ void Draw::Render(Shader Shader, glm::mat4 viewproj)
    VBO.Bind();
    EBO1.Bind();
    
-    glDrawElements(GL_TRIANGLES,indices.size(), GL_UNSIGNED_INT, 0);
-
+   glDrawElements(GL_TRIANGLES,indices.size(), GL_UNSIGNED_INT, 0);
+   // glDrawArrays(GL_POINT, 0, vertices.size());
     //unbind
     VAO.Unbind();
    VBO.Unbind();
